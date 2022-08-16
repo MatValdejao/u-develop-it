6e7a26fc885c94394ddaf99be419a297767f48df
MatValdejao <matvaldejao123@gmail.com>
2022-08-16 17:08:12 -0400

    added database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mysql = require("mysql2")
 
 const PORT = 3001 || process.env.PORT;
 const app = express()
@@ -6,6 +7,23 @@ const app = express()
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // MySQL username
+        user: "root",
+        // SQL password
+        password: "19Mateus@",
+        database: "election"
+    },
+    console.log("connected to the database election")
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows)
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
